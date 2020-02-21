dd94a996eb2777b2fd24f07687a5f4aa9c4a109e
Paul von Hagen <pvonhagen26@gmail.com>
2020-02-21 04:28:41 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,7 +4,7 @@ require 'pry'
   # contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
+  #     email: "jon_snow@thewall.we",
   #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
   #   },
   #   "Freddy Mercury" => {
@@ -13,7 +13,15 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-  
-def remove_strawberry(contacts)
 
+def remove_strawberry(contacts)
+  contacts.each do |person, data|
+    if person == "Freddy Mercury"
+      data.each do |item, attribute|
+        if item == :favorite_ice_cream_flavors
+          attribute.delete_if {|flavor| flavor == "strawberry"}
+        end
+      end
+    end
+  end
 end
